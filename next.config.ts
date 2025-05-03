32936c84af7614ad0ec76fbf047c3c573e1429a0
kapilsuham <kapilsuham@gmail.com>
2025-05-03 18:36:55 +0530

    updated articles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+    // NextJS <Image> component needs to whitelist domains for src={}
+    // It's set to allow all bt default
+    {
+      protocol: 'https',
+      hostname: '**',
+      port: '',
+      pathname: '/**',
+    }
+  ],
+},
 };
-
 export default nextConfig;
